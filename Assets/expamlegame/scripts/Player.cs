662ff7f66e6382094d525beded1a9aee7e5da3c2
calum <calum36377@hotmail.co.uk>
2018-02-26 11:25:32 +0000

    expamleplayercode

--- Assets/expamlegame/scripts/Player.cs
+++ Assets/expamlegame/scripts/Player.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("Hello world!");
+    public string logText = "hello again world";
+    public float speed = 2;
+
+    // Use this for initialization
+    void Start () {
+        
+       Debug.Log(logText);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+            Debug.Log(logText);
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        
+        rigidbody.velocity = Vector2.right * speed; 
+       
+
+
+    }
 }
+
