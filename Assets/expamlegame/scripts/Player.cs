d85f2ede6ee113feef1e94dcd577daf42786f664
calum <calum36377@hotmail.co.uk>
2018-02-26 12:51:33 +0000

    adding enemes and damage

--- Assets/expamlegame/scripts/Player.cs
+++ Assets/expamlegame/scripts/Player.cs
@@ -5,17 +5,18 @@ public class Player : MonoBehaviour {
 
     public string logText = "hello again world";
     public float speed = 2;
+    public float health = 10;
 
     // Use this for initialization
     void Start () {
         
        Debug.Log(logText);
-
+        ApplyDamage(0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-            Debug.Log(logText);
+            
         Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
         
         rigidbody.velocity = Vector2.right * speed; 
@@ -23,5 +24,14 @@ public class Player : MonoBehaviour {
 
 
     }
+
+    public void ApplyDamage (float damagetodeal)
+    {
+        health = health - damagetodeal;
+
+
+    }
+
+
 }
 
